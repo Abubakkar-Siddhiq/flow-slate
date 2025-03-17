8de6b1b96dae366872cd786358977b893bda5059
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-03-17 09:04:52 +0530

    Updated Layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
     <div className="flex gap-4">
       <h1>For Auth users only!</h1>
-      <div className="">
-        <UserButton />
-      </div>
     </div>
   );
 }
