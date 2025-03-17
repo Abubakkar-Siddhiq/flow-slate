8c0e4f2ced8c14c9f1e01ff99f9ba3f6d0d6a2c9
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-03-17 07:25:41 +0530

    Added Sidebar

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,8 @@
+export const Sidebar = () => {
+  return (
+    <aside className="fixed left-0 top-0 h-full w-[60px] bg-blue-900 text-white flex flex-col p-3 gap-y-4 z-[1]">
+      {/* <Sidebar /> */}
+      Sidebar
+    </aside>
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <div className="flex gap-4">
+      <h1>For Auth users only!</h1>
+      <div className="">
+        <UserButton />
+      </div>
+    </div>
+  );
+}
