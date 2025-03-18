6d68772c2a1abb9a1a656480d37befdb68b88f8b
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-03-18 17:36:07 +0530

    Added tooltip to Sidebar icons

--- app/(dashboard)/_components/new-button.tsx
+++ app/(dashboard)/_components/new-button.tsx
@@ -3,15 +3,18 @@
 import { CreateOrganization } from "@clerk/nextjs";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Plus } from "lucide-react";
+import { Hint } from "./hint";
 
 export const NewButton = () => {
     return (
         <Dialog>
             <DialogTrigger asChild>
                 <div className="aspect-square">
-                    <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition cursor-pointer">
-                        <Plus className="text-white" />
-                    </button>
+                    <Hint label="Create new organization" side="right" align="start">
+                        <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition cursor-pointer">
+                            <Plus className="text-white" />
+                        </button>
+                    </Hint>
                 </div>
             </DialogTrigger>
             <DialogContent>
