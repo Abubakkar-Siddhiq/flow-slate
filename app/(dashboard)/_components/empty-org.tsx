46d8202517379d684411aca41e6924a1a4a8c486
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-03-21 23:50:50 +0530

    Added EmptyOrg Component to display when the organization is empty

---
+++ app/(dashboard)/_components/empty-org.tsx
@@ -0,0 +1,8 @@
+
+export default function EmptyOrg() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+        {/* <Image src="/empty-org.svg" width={200} height={200} /> */}
+    </div>
+  )
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
+import EmptyOrg from "./_components/empty-org";
+
 export default function DashboardPage() {
   return (
-    <div className="flex gap-4">
-      <h1>For Auth users only!</h1>
+    <div className="bg-red-400 flex-1 h-[calc(100%-80px)] p-6">
+      <EmptyOrg />
     </div>
   );
 }
