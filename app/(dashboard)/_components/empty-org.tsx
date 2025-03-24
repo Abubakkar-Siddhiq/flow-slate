f10ffe3d364ba1116fbb174304ad6bb69d8c589a
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-03-24 23:56:41 +0530

    Added Create Organization Button

--- app/(dashboard)/_components/empty-org.tsx
+++ app/(dashboard)/_components/empty-org.tsx
@@ -1,8 +1,32 @@
-
+import { Button } from "@/components/ui/button"
+import { CreateOrganization } from "@clerk/nextjs"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger
+} from "@/components/ui/dialog"
 export default function EmptyOrg() {
   return (
     <div className="h-full flex flex-col items-center justify-center">
         {/* <Image src="/empty-org.svg" width={200} height={200} /> */}
+        <h1 className="text-2xl font-semibold mt-6">
+          Welcome to Board
+        </h1>
+        <p className="text-muted-foreground text-sm mt-2">
+          Create an organization to get Started
+        </p>
+        <div className="mt-6">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="lg">
+                Create an organization
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+              <CreateOrganization />
+            </DialogContent>
+          </Dialog>
+        </div>
     </div>
   )
 }
