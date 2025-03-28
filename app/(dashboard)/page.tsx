1d6d673dfd7301f23d32b7de40f6134a8da56253
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-03-28 23:51:45 +0530

    Added BoardList component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,32 @@
-import EmptyOrg from "./_components/empty-org";
+"use client";
 
-export default function DashboardPage() {
+import { useOrganization } from "@clerk/nextjs";
+import EmptyOrg from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+};
+
+export default function DashboardPage({
+  searchParams,
+}: DashboardPageProps) {
+  const { organization } = useOrganization();
+
   return (
-    <div className="bg-red-400 flex-1 h-[calc(100%-80px)] p-6">
-      <EmptyOrg />
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList
+          orgId={organization.id}
+          query={searchParams}
+        />
+      )
+      }
     </div>
   );
 }
